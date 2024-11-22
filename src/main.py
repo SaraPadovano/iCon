0b602135bdb340c59b0519ccc1f28649c8a239af
SaraPadovano <padovano.sara23@gmail.com>
2024-11-22 20:15:26 +0100

    inserite le parti iniziali per il feature engineering

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from auto_prolog import write_creators, write_auto_info
+from features import recent
 
 # Percorso dei file
 fileName = "../dataset/Automobile.csv"
 fileName_cleaned = "../dataset/Automobile_cleaned.csv"
+fileName_features = "../dataset/Automobile_features.csv"
 file_know_base = "kb.pl"
 
 # pulizia del file da eventuali dati mancanti
@@ -13,9 +15,23 @@ try:
     # Elimina le righe con valori mancanti dal dataset
     df_cleaned = df.dropna()
 
+    # Controlla che le colonne numeriche siano effettivamente numeriche e eventualmente le converte
+    numeric_columns = [
+        'mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model_year', 'price'
+    ]
+    for column in numeric_columns:
+        df_cleaned[column] = pd.to_numeric(df[column], errors='coerce')
+
     # Salva il file pulito in un nuovo file
     df_cleaned.to_csv(fileName_cleaned, index=False)
     print(f"File saved successfully to {fileName_cleaned}")
+
+    # Salvo il file completamente ripulito in un altro file per il feature engineering
+    df_cleaned.to_csv(fileName_features, index=False)
+    df_features = pd.read_csv(fileName_features, on_bad_lines='skip')
+
+    # Aggiungiamo la colonna recent al file ripulito
+    recent(df_features)
 
 except FileNotFoundError:
     print(f"Error: The file {fileName} was not found.")
@@ -25,7 +41,7 @@ except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
 
-#RAGIONAMENTO LOGICO
+# RAGIONAMENTO LOGICO
 write_creators(fileName_cleaned)
 write_auto_info(fileName_cleaned, file_know_base)
 
