14ed7b50715d2220f1a3bc68ea6273889262ff06
SaraPadovano <padovano.sara23@gmail.com>
2024-11-27 19:20:27 +0100

    aggiunto script per inserire il codice dell'apprendimento supervisionato

---
+++ src/supervised_learning.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold, learning_curve, train_test_split, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+
