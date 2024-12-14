a378bb0114026d4898a582c8e21432cdb48a45c8
SaraPadovano <padovano.sara23@gmail.com>
2024-12-14 19:33:18 +0100

    rimozione import di libreria non usata più perchè usata per adasyn ma inutile per smogn

--- src/oversampling.py
+++ src/oversampling.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import KBinsDiscretizer
 import smogn
 
 def oversampling_smogn(X, y, target):
     X, y = np.asarray(X), np.squeeze(np.asarray(y))
     train_data = pd.concat([pd.DataFrame(X), pd.Series(y, name=target)], axis=1)
 
-    train_data_resampled = smogn.smoter(
-        data=train_data,
-        y=target,
-        samp_method='extreme'
-    )
+    train_data_resampled = smogn.smoter(data=train_data, y=target, samp_method='extreme')
     X = train_data_resampled.drop(columns=[target]).to_numpy()
     y = train_data_resampled[target].to_numpy()
     X = pd.DataFrame(X)
